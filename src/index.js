36e5a6cd47a46ec7e2e3bc7dba0f290157e1db20
jacobwmorris <jacobwmorris@gmail.com>
2022-12-05 18:38:19 -0500

    Add form to sample page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import {TodoItem, Project} from "./todos.js";
 /* let d = new Date(2000, 0, 1);
 console.log(format(d, "MM/dd/yyyy")); */
 
-const testProj = Project("test");
-testProj.add(TodoItem("do thing", "things", Date(1999, 11, 31), 2));
-testProj.add(TodoItem("do another thing", "things", Date(1999, 11, 30), 1));
-testProj.update()
+(function() {
+    const projects = [];
+
+    projects.push(Project("Default"));
+})();
