c40db84bdd950625e5f731e1de141503999e4b24
jacobwmorris <jacobwmorris@gmail.com>
2022-12-10 14:59:22 -0500

    Add todo display class

--- src/display.js
+++ src/display.js
@@ -1,9 +1,6 @@
-const DisplayProjectList = function() {
-    const projectList = document.getElementById("project-list");
-    /* const projectTitle = document.getElementById("project-title");
-    const todoList = document.getElementById("todo-list");
-    const expanded = document.getElementById("expanded"); */
+import {format} from "date-fns";
 
+const DomFuncs = (function() {
     const clearChildren = function(element) {
         while (element.children.length) {
             element.removeChild(element.lastChild);
@@ -20,10 +17,21 @@ const DisplayProjectList = function() {
         return e;
     };
 
+    return {clearChildren, makeElement};
+})();
+
+const DisplayProjectList = function() {
+    const projectList = document.getElementById("project-list");
+    /* const projectTitle = document.getElementById("project-title");
+    const todoList = document.getElementById("todo-list");
+    const expanded = document.getElementById("expanded"); */
+
+    
+
     const makeProjectListItem = function(proj, projects) {
-        const item = makeElement("li", "", "project");
-        const remove = makeElement("button", "X", "red-button");
-        const expand = makeElement("button", proj.name, "project-expand");
+        const item = DomFuncs.makeElement("li", "", "project");
+        const remove = DomFuncs.makeElement("button", "X", "red-button");
+        const expand = DomFuncs.makeElement("button", proj.name, "project-expand");
 
         remove.addEventListener("click", function(event) {
             projects.remove(proj);
@@ -34,17 +42,88 @@ const DisplayProjectList = function() {
         return item;
     };
 
-    const update = function(projects) {
+    const update = function(projectListData) {
         const list = document.createElement("ul");
-        for (let i = 0; i < projects.projCount(); i++) {
-            const proj = projects.get(i);
-            list.appendChild(makeProjectListItem(proj, projects));
+        for (let i = 0; i < projectListData.projCount(); i++) {
+            const proj = projectListData.get(i);
+            list.appendChild(makeProjectListItem(proj, projectListData));
         }
-        clearChildren(projectList);
+        DomFuncs.clearChildren(projectList);
         projectList.appendChild(list);
     };
 
     return {update};
 };
 
-export {DisplayProjectList};
+const DisplayTodo = function() {//Do we construct this with a todo object?
+    const todo = DomFuncs.makeElement("div", "", "todo");
+
+    const makeTitleDesc = function(title, desc) {
+        const titleDesc = DomFuncs.makeElement("div", "", "todo-titledesc");
+        titleDesc.appendChild(DomFuncs.makeElement("h2", title));
+        titleDesc.appendChild(DomFuncs.makeElement("p", desc));
+        return titleDesc;
+    };
+
+    const update = function(todoData) {
+        todo.setAttribute("data-priority", todoData.priority);
+
+        if (todoData.checked)
+            todo.classList.add("checked");
+        else
+            todo.classList.remove("checked");
+        
+        //Separate these into functions that apply callbacks
+        const removeButton = DomFuncs.makeElement("button", "X", "red-button");
+        const expandButton = DomFuncs.makeElement("button", ">>", "todo-expand");
+
+        //Need function to create checkbox
+
+        DomFuncs.clearChildren(todo);
+        todo.appendChild(removeButton);
+        todo.appendChild(makeTitleDesc(todoData.title, todoData.description));
+        todo.appendChild(DomFuncs.makeElement("p", format(todoData.dueDate, "MM-dd-yyyy"), "todo-date"));
+        todo.appendChild(DomFuncs.makeElement("div", todoData.priority, "todo-priority"));
+        todo.appendChild(expandButton);
+    };
+
+    const getElement = function() {return todo;};
+
+    return {update, getElement};
+};
+
+const DisplayTodoList = function() {
+    const todoList = document.getElementById("todo-list");
+    const todoItems = [];
+
+    const update = function(projectData) {
+        DomFuncs.clearChildren(todoList);
+
+        for (let i = 0; i < projectData.todoCount(); i++) {
+            const item = projectData.get(i);
+            const itemDisplay = DisplayTodo();
+            itemDisplay.update(item);
+            todoList.appendChild(itemDisplay.getElement());
+            //todoItems.push(itemDisplay);
+        }
+    };
+
+    return {update};
+};
+
+/* <div class="todo pri-low">
+const TodoItem = function(title, description, dueDate, priority, checked) {
+    return {title, description, dueDate, priority, checked};
+};
+                    <button class="red-button">X</button>
+                    <div class="todo-titledesc">
+                        <h2>Do thing</h2>
+                        <p>stuff that will need to be done to complete the thing in question.</p>
+                    </div>
+                    <p class="todo-date">12-31-2022</p>
+                    <div class="todo-priority">L</div>
+                    <input class="todo-done" type="checkbox">
+                    <button class="todo-expand">>></button>
+                </div> */
+
+export {DisplayProjectList, DisplayTodo, DisplayTodoList};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-//import {format} from "date-fns";
 import "./style.css";
 import {TodoItem, Project, ProjectList} from "./todos.js";
-import {DisplayProjectList} from "./display.js";
-
-/* let d = new Date(2000, 0, 1);
-console.log(format(d, "MM/dd/yyyy")); */
+import {DisplayProjectList, DisplayTodoList} from "./display.js";
 
 (function() {
     //Set up displayers
     const displayProjectList = DisplayProjectList();
+    const displayTodoList = DisplayTodoList();
     
     //Create project list and hook it up to display
     const projects = ProjectList();
     projects.registerObserver(displayProjectList);
 
-    //Create a default project
-    projects.add(Project("Default"));
+    //Create a default project, and some default todos to test
+    const defaultProj = Project("Default");
+    defaultProj.add(TodoItem("Test 1", "eiojckzlxnvapezmkcvpoiewalkaknmvklzxciovnrjanlkxcve", new Date(1, 0, 2000), "L", false));
+    defaultProj.add(TodoItem("Test 2", "fiuocmizliumcoqkkjziscjoqc,jnvhruiozkjxhgiopamcjkzoiiii", new Date(2, 1, 2000), "M", false));
+    defaultProj.add(TodoItem("Test 3", "acjmbioqoiejilkxio", new Date(3, 2, 2000), "H", false));
+    defaultProj.add(TodoItem("Test 4", "lzicuhjkiqumcnjvkjbuyaoiq", new Date(4, 3, 2000), "L", true));
+    displayTodoList.update(defaultProj);
+    projects.add(defaultProj);
 
     //Add form callbacks
     const projectFormShowCallback = function(event) {
@@ -35,6 +38,14 @@ console.log(format(d, "MM/dd/yyyy")); */
         nameInput.value = "";
         document.querySelector(".form-wrapper.proj-form").classList.add("no-display");
         event.preventDefault();
+    };
+
+    const addTodoCallback = function(event) {
+        const titleIn = document.getElementById("f-title");
+        const dateIn = document.getElementById("f-date");
+        const descIn = document.getElementById("f-desc");
+
+        ///How to get data from radio buttons?
     };
 
     document.getElementById("project-form-button").addEventListener("click", projectFormShowCallback);
